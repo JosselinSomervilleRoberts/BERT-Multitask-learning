35ef0e06e6d9481b16c65b94d44bcb1de997e2ce
marie-huynh <72524396+marie-huynh@users.noreply.github.com>
2023-03-02 03:17:16 -0800

    Fix bug

--- scheduler_analysis.py
+++ scheduler_analysis.py
@@ -10,13 +10,13 @@ if __name__ == '__main__':
     args.use_gpu = True
     args.save_loss_logs = True
     seed_everything(args.seed)  # fix the seed for reproducibility
-    #args.option == "pretrain"
+    #args.option = "pretrain"
     #train_multitask(args)
 
     #Now, we fine-tune the model with different schedulers
     schedulers = ['random', 'round_robin', 'pal']
     args.learning_rate = 0.0005
-    args.option == "finetune"
+    args.option = "finetune"
     print(args.option)
     for scheduler in schedulers:
         #We load the pretrained model
