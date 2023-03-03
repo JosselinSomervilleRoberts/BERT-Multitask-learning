f0acdf4b5e06e61ff1d17d597466f95e2c1229ca
marie-huynh <72524396+marie-huynh@users.noreply.github.com>
2023-03-03 13:34:17 -0800

    Generate Plot of Loss logs in scheduler_analysis.py

--- scheduler_analysis.py
+++ scheduler_analysis.py
@@ -1,28 +1,41 @@
 #This file compares the learning of multiple schedulers : PalScheduler, RandomScheduler, RoundRobinScheduler
-from multitask_classifier import *
+import matplotlib.pyplot as plt
+
+def read_logs(path):
+    """
+    Read the loss logs of a scheduler.
+    :param path: path to the log file.
+    :return: list of loss values.
+    """
+
+    with open(path, 'r') as f:
+        logs = f.readlines()
+    logs = logs[1:]
+    logs = [float(log.split(',')[1]) for log in logs]
+    return logs
+
+def compare_schedulers_logs(list_logs):
+    """
+    Compare the loss logs of multiple schedulers and generates a single graph with the loss of each scheduler.
+    :param list_logs: list of loss logs for each scheduler.
+    :return: None
+    """
+    pal_log, random_log, round_robin_log = list_logs
+    plt.figure(figsize=(10, 5))
+    plt.legend(['PalScheduler', 'RandomScheduler', 'RoundRobinScheduler'])
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.show()
 
 
-if __name__ == '__main__':
-    #First, we pretrain the model
-    args = get_args()
-    args.epochs = 12
-    args.learning_rate = 0.001
-    args.use_gpu = True
-    args.save_loss_logs = True
-    seed_everything(args.seed)  # fix the seed for reproducibility
-    #args.option = "pretrain"
-    #train_multitask(args)
+if main == '__main__':
+    pal_log_0 = read_logs('train_loss_logs_epochs_pal_0.txt')
+    random_log_0 = read_logs('train_loss_logs_epochs_random_0.txt')
+    round_robin_log_0 = read_logs('train_loss_logs_epochs_round_robin_0.txt')
 
-    #Now, we fine-tune the model with different schedulers
-    schedulers = ['random'] #, 'round_robin', 'pal']
-    args.learning_rate = 0.0005
-    args.option = "finetune"
-    print(args.option)
-    args.epochs= 1
-    for scheduler in schedulers:
-        #We load the pretrained model
-        print(f'Running {scheduler} scheduler')
-        args.task_scheduler = scheduler    
-        args.filepath = f'{args.option}-{args.epochs}-{args.lr}-{args.task_scheduler}-multitask.pt' # save path
-        train_multitask(args)
-        print(f'Finished {scheduler} scheduler')
+    pal_log_1 = read_logs('train_loss_logs_epochs_pal_1.txt')
+    random_log_1 = read_logs('train_loss_logs_epochs_random_1.txt')
+    round_robin_log_1 = read_logs('train_loss_logs_epochs_round_robin_1.txt')
+
+    print(pal_log_0)
+    #compare_schedulers_logs([pal_log, random_log, round_robin_log])
