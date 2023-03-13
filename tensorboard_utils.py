042c2dfebe3ce83ea5f8f0cb1399f8d4baee1f4d
JosselinSomervilleRoberts <josselin.somerville@polytechnique.edu>
2023-03-13 23:19:12 +0100

    New Confusion matrix writer

--- tensorboard_utils.py
+++ tensorboard_utils.py
@@ -3,16 +3,75 @@ import seaborn as sn
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import itertools
+import io
+import tensorflow as tf
+
+def plot_confusion_matrix(cm, class_names = None):
+    """
+    Returns a matplotlib figure containing the plotted confusion matrix.
+    
+    Args:
+       cm (array, shape = [n, n]): a confusion matrix of integer classes
+       class_names (array, shape = [n]): String names of the integer classes
+    """
+
+    if class_names is None:
+        class_names = [i for i in range(len(cm))]
+    
+    figure = plt.figure(figsize=(8, 8))
+    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+    plt.title("Confusion matrix")
+    plt.colorbar()
+    tick_marks = np.arange(len(class_names))
+    plt.xticks(tick_marks, class_names, rotation=45)
+    plt.yticks(tick_marks, class_names)
+    
+    # Normalize the confusion matrix.
+    cm = np.around(cm.astype('float') / cm.sum(axis=1)[:, np.newaxis], decimals=2)
+    
+    # Use white text if squares are dark; otherwise black.
+    threshold = cm.max() / 2.
+    
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        color = "white" if cm[i, j] > threshold else "black"
+        plt.text(j, i, cm[i, j], horizontalalignment="center", color=color)
+        
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    return figure
+
+
+def plot_to_image(figure):
+    """
+    Converts the matplotlib plot specified by 'figure' to a PNG image and
+    returns it. The supplied figure is closed and inaccessible after this call.
+    """
+    
+    buf = io.BytesIO()
+    
+    # Use plt.savefig to save the plot to a PNG in memory.
+    plt.savefig(buf, format='png')
+    
+    # Closing the figure prevents it from being displayed directly inside
+    # the notebook.
+    plt.close(figure)
+    buf.seek(0)
+    
+    # Use tf.image.decode_png to convert the PNG buffer
+    # to a TF image. Make sure you use 4 channels.
+    image = tf.image.decode_png(buf.getvalue(), channels=4)
+    
+    # Use tf.expand_dims to add the batch dimension
+    image = tf.expand_dims(image, 0)
+    
+    return image
 
 def createConfusionMatrix(y_true, y_pred):
-    # Classes from 0 to num_classes-1
-    num_classes = np.max(y_true) + 1
-    classes = [i for i in range(num_classes)]
-
     # Build confusion matrix
-    cf_matrix = confusion_matrix(y_true, y_pred)
-    cf_matrix /= np.sum(cf_matrix)
-    # df_cm = pd.DataFrame(cf_matrix/np.sum(cf_matrix), index=[i for i in classes],
-    #                      columns=[i for i in classes])
-    plt.figure(figsize=(2*num_classes, 2*num_classes))    
-    return sn.heatmap(cf_matrix, annot=True).get_figure()
+    cm_matrix = confusion_matrix(y_true, y_pred)
+    figure = plot_confusion_matrix(cm_matrix, class_names=None)
+    cm_image = plot_to_image(figure)
+
+    return cm_image
