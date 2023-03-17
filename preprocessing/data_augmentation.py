ecd1435afcff4cbaba8837f18d1919a23c802e0c
marie-huynh <72524396+marie-huynh@users.noreply.github.com>
2023-03-17 16:50:32 -0700

    Fixing final problem in data augmentation

--- preprocessing/data_augmentation.py
+++ preprocessing/data_augmentation.py
@@ -36,13 +36,12 @@ def generate_EDA_format(df, label_name, input_name, saving_path):
         final_df_eda_format.reset_index()
     if (label_name == "is_duplicate"):
         nb_1 = class_to_augment[1]//2
-        class_1 = df_eda[df_eda["is_duplicate"] == 0.0].head(nb_1)
+        class_1 = df_eda[df_eda["is_duplicate"] == 1.0].head(nb_1)
         final_df_eda_format = class_1
-        class_1["merged output"] = class_1["sentence1"] + " [SEP] " + class_1["sentence2"]
-        print(class_1.head())
+        class_1["merged output"] = class_1["sentence1"] + " x1x " + class_1["sentence2"]
         class_1.drop(columns=["sentence1", "sentence2"], inplace=True)
-        print(class_1.head())
         final_df_eda_format.reset_index()
+        
     #We save to csv file
     final_df_eda_format.to_csv(saving_path, sep='\t', index=False, header=False)
 
@@ -51,18 +50,20 @@ def merge_augmented_data(df, augmented_data_path, label_name, input_name, saving
     This function merges the augmented data to the original dataset."""
     #We read the augmented data
     df_augmented = pd.read_csv(augmented_data_path, sep='\t', header=None, index_col=None)
-
     #We rename the columns
     if (label_name == "sentiment"):
         df_augmented.rename(columns={0:label_name, 1:input_name[0]}, inplace=True)
     if (label_name == "is_duplicate"):
-        print(df_augmented.head())
-        separator = " sep "
-        df_augmented[['sentence1', 'sentence2']] = df_augmented[1].str.split(separator, expand=True)
-        print(df_augmented.columns)
-        df_augmented.rename(columns={0:label_name}, inplace=True)
-        df_augmented.drop(columns=[1], inplace=True)
+        separator = " x x "
+        # Create a boolean mask that indicates which rows contain " x x "
+        mask = df_augmented[1].str.contains(" x x ")
+        # Use the mask to filter the dataframe
+        df_augmented = df_augmented.loc[mask]
+        df_augmented = df_augmented.reset_index(drop=True)
 
+        # Count the number of rows that contain " x x "
+        df_augmented[['sentence1', 'sentence2']] = df_augmented[1].str.split(separator, expand=True)
+        df_augmented.rename(columns={0:label_name}, inplace=True)
 
     #We concatenate the dataframes
     df_merged = pd.concat([df, df_augmented])
@@ -80,6 +81,9 @@ if __name__ == "__main__":
     df_train_paraphrase = pd.read_csv('data/preprocessed_data/lengths/preprocessed-quora-train.csv', sep='\t', header=0, index_col=0)
     save_path_paraphrase = 'data/preprocessed_data/EDA_data/input-EDA-format-quora-train.txt'
 
+    print(df_train_sentiment["sentiment"].value_counts())
+    print(df_train_paraphrase["is_duplicate"].value_counts())
+
     #We generate the EDA format needed for the data augmentation
     generate_EDA_format_flag = False
     if generate_EDA_format_flag:
@@ -94,7 +98,13 @@ if __name__ == "__main__":
     augmented_data_path_paraphrase = 'data/preprocessed_data/EDA_data/output-EDA-format-quora-train.txt'
     save_path_paraphrase = 'data/preprocessed_data/EDA_data/preprocessed-EDA-quora-train.csv'
 
-    merge_augmented_data(df_train_sentiment, augmented_data_path_sentiment, 'sentiment', ['sentence'], save_path_sentiment)
-    merge_augmented_data(df_train_paraphrase, augmented_data_path_paraphrase, 'is_duplicate', ['sentence1', 'sentence2'], save_path_paraphrase)
-
-
+    merge_augmented_data_flag = True
+    if merge_augmented_data_flag:
+        merge_augmented_data(df_train_sentiment, augmented_data_path_sentiment, 'sentiment', ['sentence'], save_path_sentiment)
+        merge_augmented_data(df_train_paraphrase, augmented_data_path_paraphrase, 'is_duplicate', ['sentence1', 'sentence2'], save_path_paraphrase)
+    
+    #We read the augmented data
+    df_augmented_sentiment = pd.read_csv('data/preprocessed_data/EDA_data/preprocessed-EDA-ids-sst-train.csv', sep='\t', header=0, index_col=0)
+    df_augmented_paraphrase = pd.read_csv('data/preprocessed_data/EDA_data/preprocessed-EDA-quora-train.csv', sep='\t', header=0, index_col=0)
+    print(df_augmented_sentiment["sentiment"].value_counts())
+    print(df_augmented_paraphrase["is_duplicate"].value_counts())
