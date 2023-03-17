203decfb751279b72773dcc3a90f3133b0012074
marie-huynh <72524396+marie-huynh@users.noreply.github.com>
2023-03-16 23:31:37 -0700

    updated EDA sst dataset

--- preprocessing/data_augmentation.py
+++ preprocessing/data_augmentation.py
@@ -12,13 +12,11 @@ def generate_EDA_format(df, label_name, input_name, saving_path):
     df_eda = df[[label_name] + input_name]
     #We get the counts of each label in the dataset
     counts = df_eda[label_name].value_counts()
-    print(counts)
     most_common_label = counts.idxmax()
     possible_labels = df_eda[label_name].unique()
     class_to_augment = dict()
     for label in possible_labels:
         class_to_augment[label] = counts[most_common_label] - counts[label]
-        print(class_to_augment[label])
     #We want to augment the classes that are underrepresented
     #We shuffle once the dataframe
     df_eda = df_eda.sample(frac=1).reset_index(drop=True)
@@ -52,18 +50,17 @@ def generate_EDA_format(df, label_name, input_name, saving_path):
 def merge_augmented_data(df, augmented_data_path, label_name, input_name, saving_path, classes_to_augment):
     """
     This function merges the augmented data to the original dataset."""
-    #We drop the underrepresented classes
-    print(len(df))
-    df = df[~df[label_name].isin(classes_to_augment)]
-    print(len(df))
     #We read the augmented data
     df_augmented = pd.read_csv(augmented_data_path, sep='\t', header=None, index_col=None)
     #We rename the columns
     df_augmented.rename(columns={0:label_name, 1:input_name}, inplace=True)
-    print(len(df_augmented))
-    #We merge the dataframes
-    df_merged = pd.concat([df, df_augmented], ignore_index=True)
-    #df_merged.to_csv(saving_path, sep='\t', header=True)
+    #We concatenate the dataframes
+    df_merged = pd.concat([df, df_augmented])
+    #We shuffle the dataframe
+    df_merged = df_merged.sample(frac=1).reset_index(drop=True)
+    #We drop the duplicates having the same "sentence"
+    df_merged.drop_duplicates(subset=[input_name], keep='first', inplace=True)
+    df_merged.to_csv(saving_path, sep='\t', header=True)
 
 if __name__ == "__main__":
     #We read the datasets
@@ -80,8 +77,8 @@ if __name__ == "__main__":
         #generate_EDA_format(df_train_paraphrase, 'is_duplicate', ['sentence1', 'sentence2'], save_path_paraphrase)
 
     #We merge the augmented data generated using the code of the following repo (source : https://github.com/jasonwei20/eda_nlp)
-    # to the original dataset
-    augmented_data_path = 'data/preprocessed_data/EDA_data/output-EDA-format-ids-sst-train_augmented.txt'
+    #to the original dataset
+    augmented_data_path = 'data/preprocessed_data/EDA_data/output-EDA-format-ids-sst-train.txt'
     save_path = 'data/preprocessed_data/EDA_data/preprocessed-EDA-ids-sst-train.csv'
     merge_augmented_data(df_train_sentiment, augmented_data_path, 'sentiment', 'sentence', save_path, [0,2,4])
 
