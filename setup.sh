6c61533091b52c7fa739668bfdecf723979ce680
marie-huynh <72524396+marie-huynh@users.noreply.github.com>
2023-03-24 15:38:51 -0700

    fix torch issue

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 
 conda create -n cs224n_dfp2 python=3.8
 conda activate cs224n_dfp2
-
+pip install pytorch==1.8.0 torchvision torchaudio cudatoolkit=10.1 -c pytorch
 pip install -r requirements.txt
