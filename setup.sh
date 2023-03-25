2193c12775ec0d5c65fdc0c94babce59ad7a129a
marie-huynh <72524396+marie-huynh@users.noreply.github.com>
2023-03-24 22:47:59 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 
-conda create -n cs224n_dfp2 python=3.8
-conda activate cs224n_dfp2
+conda create --name cs224n_dfp_final --file spec-file.txt
+conda activate cs224n_dfp_final
 
-conda install pytorch==1.8.0 torchvision torchaudio cudatoolkit=10.1 -c pytorch
-pip install tqdm==4.58.0
-pip install requests==2.25.1
-pip install importlib-metadata==3.7.0
-pip install filelock==3.0.12
-pip install sklearn==0.0
-pip install tokenizers==0.10.1
-pip install explainaboard_client==0.0.7
 pip install -r requirements.txt
